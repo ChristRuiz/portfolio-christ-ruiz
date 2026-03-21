7215069b7f33e00783012dfe65a37be2ec661a14
Christian Ruiz <chris.r.c911@gmail.com>
2026-03-21 23:41:57 +0100

    Se optimiza la imagen y el menu

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const site = {
   title: "Christian Ruiz | Desarrollador Full Stack",
   description:
     "Portfolio de Christian Ruiz. Desarrollador Full Stack con más de 5 años de experiencia especializado en Angular y Java, creando soluciones modernas y escalables.",
-  image: "/foto_perfil.png",
+  image: "/foto_perfil.webp",
   email: "chris.r.c911@gmail.com",
   linkedinUrl: "https://www.linkedin.com/in/christian-ruiz-castillero/",
   efsetCertificateUrl: "https://cert.efset.org/4bAhHq",
